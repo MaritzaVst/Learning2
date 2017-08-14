c7c9b0d6ac7fc6b767e6b23aed4805c58b28637d
maritzavst <maritzasintitafur@gmail.com>
2017-08-14 17:03:11 -0500

    avance

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic
+// API key	b58f594a4e749c834088fa20857a88a1
+// Shared secret	b75ec0e6792ea5d33d9a0d0438a92624
+// Registered to	maritzavst
+
+export default {
+    appname: 'platzimusic',
+    apiKey: 'b58f594a4e749c834088fa20857a88a1',
+    secret: 'b75ec0e6792ea5d33d9a0d0438a92624',
+    registeredTo: 'maritzavst'
+}
