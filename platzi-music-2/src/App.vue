db6974c26141e0210d873d3eef0228be1ab825fd
maritzavst <maritzasintitafur@gmail.com>
2017-08-22 17:10:38 -0500

    integracion con eslint

---
+++ platzi-music-2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Vue :D !'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
