25b8427198531fdd92e6d0e7fba7f42be80cf995
maritzavst <maritzasintitafur@gmail.com>
2017-08-24 17:16:27 -0500

    commet db

--- learn-mongo/server.js
+++ learn-mongo/server.js
@@ -36,3 +36,7 @@ app.get('/books', function(req, res){
 app.listen(3000)
 
 console.log('Running')
+
+/*
+[{"_id":"599f41f2102ad5bdf679c84c","title":"Harry Potter","genre":"fantasy","description":"This volume draws together a range of Chomsky's writings arguing that the goal of education is to produce free human beings whose values are not accumulation and domination, but rather free association on terms of equality.","author":"J. Rowling","image_url":"https://covers.openlibrary.org/b/id/240726-L.jpg","publisher":"Norma","saludo":"Holi boli :3"},{"_id":"599f43a9102ad5bdf679c84e","title":"El Señor de los elfos","genre":"comedy","description":"This volume draws together a range of Chomsky's writings arguing that the goal of education is to produce free human beings whose values are not accumulation and domination, but rather free association on terms of equality.","author":"Tolkin","image_url":"https://covers.openlibrary.org/b/id/240726-L.jpg","publisher":"Nature Republic","saludo":"Holi boli :3"}]
+*/
